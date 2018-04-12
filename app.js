2cb3d46ccb60c454285ef60de12e9f5d84b90f8a
Marc Jansing <marc.jansing@innoq.com>
2018-04-12 15:26:50 +0200

    split app into controllers

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const SSE = require('express-sse')
-const EventSource = require('eventsource')
-const fetch = require('node-fetch')
+
+const homeController = require('./lib/controller/home')
+const blocksController = require('./lib/controller/blocks')
+const transactionsController = require('./lib/controller/transactions')
+const miningController = require('./lib/controller/mining')
+const nodesController = require('./lib/controller/nodes')
 
 module.exports = (blockchain) => {
   const app = express()
@@ -10,100 +14,13 @@ module.exports = (blockchain) => {
 
   app.use(bodyParser.json())
 
-  app.get('/', (req, res) => {
-    console.log('GET / invoked')
-
-    res.json({
-      nodeId: blockchain.nodeId,
-      currentBlockHeight: blockchain.blocks.length,
-      neighbours: blockchain.nodes
-    })
-  })
-
-  app.get('/chain', (req, res) => {
-    console.log('GET /chain invoked')
-
-    res.json({
-      blocks: blockchain.blocks,
-      blockHeight: blockchain.blocks.length
-    })
-  })
-
+  app.get('/', (req, res) => homeController.index(blockchain, req, res))
   app.get('/events', sse.init)
-
-  app.get('/mine', (req, res) => {
-    console.log('GET /mine invoked')
-
-    const previousBlock = blockchain.previousBlock()
-    const previousProof = previousBlock.proof
-    const proof = blockchain.proofOfWork(previousProof)
-
-    const previousBlockHash = blockchain.hash(previousBlock)
-    const newBlock = blockchain.newBlock(proof, previousBlockHash)
-
-    console.log('We found a new block', newBlock)
-    sse.send(newBlock, 'new_block')
-
-    res.json({
-      message: 'New block found',
-      block: newBlock
-    })
-  })
-
-  app.post('/nodes/register', (req, res) => {
-    console.log('POST /nodes/register invoked')
-
-    const { host } = req.body || []
-
-    fetch(host)
-      .then(res => {
-        if (res.ok) return res.json()
-      })
-      .then(fetchedNode => {
-        const node = blockchain.registerNode(fetchedNode.nodeId, host)
-
-        // connect to new node event stream
-        const eventSource = `${host}/events`
-        const stream = new EventSource(eventSource)
-        stream.addEventListener('new_block', (event) => {
-          console.log(`node ${host} found a new block: `, event.data)
-          blockchain.updateChain()
-        })
-
-        res.status(201).json({
-          message: 'New node added',
-          node
-        })
-      }).catch(err => {
-        console.error('An error occured', err)
-        res.status(400).json({
-          message: 'Error: could not add node'
-        })
-      })
-  })
-
-  app.get('/nodes/resolveChain', (req, res) => {
-    console.log('GET /nodes/resolveChain invoked')
-
-    blockchain.updateChain()
-      .then((updatedChain) => {
-        if (updatedChain) {
-          res.json({
-            message: 'Our chain was updated with a newer one.',
-            newChain: blockchain.blocks
-          })
-        } else {
-          res.json({
-            message: 'Our chain is up to date.',
-            chain: blockchain.blocks
-          })
-        }
-      })
-      .catch((err) => {
-        console.error('An error occured', err)
-        res.status(502).send('Could not update chain.')
-      })
-  })
+  app.get('/blocks', (req, res) => blocksController.index(blockchain, req, res))
+  app.get('/mine', (req, res) => miningController.mine(blockchain, sse, req, res))
+  app.get('/transactions', (req, res) => transactionsController.get(blockchain, req, res))
+  app.post('/nodes/register', (req, res) => nodesController.register(blockchain, req, res))
+  app.get('/nodes/resolveChain', (req, res) => nodesController.resolveChain(blockchain, req, res))
 
   return app
 }
