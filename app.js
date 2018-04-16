efc840c35e6a137014ea89c39a7488fd29eed177
Marc Jansing <marc.jansing@innoq.com>
2018-04-16 12:56:08 +0200

    roll back to express-sse

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const SseChannel = require('sse-channel')
+const SSE = require('express-sse')
 
 const homeController = require('./lib/controller/home')
-const eventsController = require('./lib/controller/events')
 const blocksController = require('./lib/controller/blocks')
 const transactionsController = require('./lib/controller/transactions')
 const miningController = require('./lib/controller/mining')
@@ -11,12 +10,12 @@ const nodesController = require('./lib/controller/nodes')
 
 module.exports = (blockchain) => {
   const app = express()
-  const sse = new SseChannel({jsonEncode: true})
+  const sse = new SSE(blockchain.blocks)
 
   app.use(bodyParser.json())
 
   app.get('/', (req, res) => homeController.index(blockchain, req, res))
-  app.get('/events', (req, res) => eventsController.index(blockchain, sse, req, res))
+  app.get('/events', sse.init)
   app.get('/blocks', (req, res) => blocksController.index(blockchain, req, res))
   app.get('/blocks/:id', (req, res) => blocksController.get(blockchain, req, res))
   app.get('/mine', (req, res) => miningController.mine(blockchain, sse, req, res))
